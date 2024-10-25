4291746f3bb760f8a4a93103489643ed52f25859
Pritesh Chaturvedi <priteshchoubey567@gmail.com>
2024-10-25 21:44:50 +0530

    Create MathMethods.java

---
+++ MathMethods.java
@@ -0,0 +1,15 @@
+public class MathMethods {
+    public static void main(String[] args) {
+        
+        System.out.println(Math.sqrt(64));//square root
+        System.out.println(Math.min(10, 12));//minimum of two
+        System.out.println(Math.random());//any random no. in decimals
+        //random no. between 1 to 10
+        System.out.println((int) (Math.random()*10) +1);
+        System.out.println(Math.abs(-11));//absolute value 11
+        System.out.println(Math.pow(2,3));//2 raise to power 3 is 8
+        System.out.println(Math.floor(14.2));//just lower number 14
+        System.out.println(Math.ceil(14.2));//just upper number 15
+        
+    }
+}
